524932a5f7948775a370fe54e9db0ed05bcc631e
Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
2019-08-24 18:52:03 -0500

    remove mistaken imports of google maps

--- app/src/main/java/com/sweetiepiggy/buswhentwincities/MyMapFragment.kt
+++ app/src/main/java/com/sweetiepiggy/buswhentwincities/MyMapFragment.kt
@@ -37,13 +37,6 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
 import java.util.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
