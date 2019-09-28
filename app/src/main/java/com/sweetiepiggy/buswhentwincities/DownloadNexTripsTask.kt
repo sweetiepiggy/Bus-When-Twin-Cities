c43abf01a07b4a9f3c7c11fded1f9aeda8b198c8
Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
2019-09-28 02:05:34 -0500

    support https on old Android versions by trusting server certificate
    it seems http support at svc.metrotransit.org has been removed

--- app/src/main/java/com/sweetiepiggy/buswhentwincities/DownloadNexTripsTask.kt
+++ app/src/main/java/com/sweetiepiggy/buswhentwincities/DownloadNexTripsTask.kt
@@ -20,12 +20,19 @@
 package com.sweetiepiggy.buswhentwincities
 
 import android.os.AsyncTask
+import android.os.Build
 import android.util.JsonReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.io.UnsupportedEncodingException
-import java.net.*
+import java.io.*
+import java.net.MalformedURLException
+import java.net.SocketException
+import java.net.URL
+import java.net.UnknownHostException
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
 import java.util.*
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 class DownloadNexTripsTask(private val mDownloadedListener: OnDownloadedListener,
                            private val mStopId: Int) : AsyncTask<Void, Int, Void>() {
@@ -40,44 +47,26 @@ class DownloadNexTripsTask(private val mDownloadedListener: OnDownloadedListener
     inner class UnauthorizedException : IOException()
 
     override fun doInBackground(vararg params: Void): Void? {
-        var retry: Boolean
-        var firstError: DownloadError? = null
         var rawNexTrips: List<RawNexTrip>? = null
 
-        do {
-            retry = false
-            try {
-                rawNexTrips = downloadRawNexTrips(mStopId)
-            } catch (e: UnknownHostException) { // probably no internet connection
-            	mError = DownloadError.UnknownHost
-            } catch (e: java.io.FileNotFoundException) {
-                mError = DownloadError.FileNotFound(e.message)
-            } catch (e: java.net.SocketTimeoutException) {
-                mError = DownloadError.TimedOut(e.message)
-            } catch (e: UnauthorizedException) {
-                mError = DownloadError.Unauthorized
-            } catch (e: SocketException) {
-                mError = DownloadError.OtherDownloadError(e.message)
-            } catch (e: MalformedURLException) {
-                mError = DownloadError.OtherDownloadError(e.message)
-            } catch (e: UnsupportedEncodingException) {
-                mError = DownloadError.OtherDownloadError(e.message)
-            } catch (e: IOException) {
-                if (firstError == null) firstError = DownloadError.OtherDownloadError(e.message)
-                // old Android versions seem to have a problem with https and
-                // throw IOException: CertPathValidatorException,
-                // try again using http
-                if (mUseHttps) {
-                    mUseHttps = false
-                    retry = true
-                } else {
-                    mError = DownloadError.OtherDownloadError(e.message)
-                }
-            }
-        } while (retry && !isCancelled())
-
-        if (mError != null && firstError != null) {
-            mError = firstError
+        try {
+            rawNexTrips = downloadRawNexTrips(mStopId)
+        } catch (e: UnknownHostException) { // probably no internet connection
+        	mError = DownloadError.UnknownHost
+        } catch (e: java.io.FileNotFoundException) {
+            mError = DownloadError.FileNotFound(e.message)
+        } catch (e: java.net.SocketTimeoutException) {
+            mError = DownloadError.TimedOut(e.message)
+        } catch (e: UnauthorizedException) {
+            mError = DownloadError.Unauthorized
+        } catch (e: SocketException) {
+            mError = DownloadError.OtherDownloadError(e.message)
+        } catch (e: MalformedURLException) {
+            mError = DownloadError.OtherDownloadError(e.message)
+        } catch (e: UnsupportedEncodingException) {
+            mError = DownloadError.OtherDownloadError(e.message)
+        } catch (e: IOException) {
+            mError = DownloadError.OtherDownloadError(e.message)
         }
 
         if (!isCancelled() && rawNexTrips != null) {
@@ -99,9 +88,30 @@ class DownloadNexTripsTask(private val mDownloadedListener: OnDownloadedListener
     private fun downloadRawNexTrips(stopId: Int): List<RawNexTrip>? {
         var rawNexTrips: List<RawNexTrip>?
 
-        val nexTripsUrl = ((if (mUseHttps) "https://" else "http://")
-                + NEXTRIPS_URL + stopId.toString() + "?format=json")
-        val urlConnection = URL(nexTripsUrl).openConnection() as HttpURLConnection
+        val nexTripsUrl = NEXTRIPS_URL + stopId.toString() + "?format=json"
+        val urlConnection = (URL(nexTripsUrl).openConnection() as HttpsURLConnection).apply {
+            // trust svc.metrotransit.org server certificate on Android 4.1 - 4.4
+            // https://developer.android.com/training/articles/security-ssl
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+                val caInput: InputStream = ByteArrayInputStream(SERVER_CERTIFICATE.toByteArray())
+                val ca = caInput.use {
+                    cf.generateCertificate(it)
+                }
+                val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
+                    load(null, null)
+                    setCertificateEntry("ca", ca)
+                }
+                val tmf =
+                	TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
+                        init(keyStore)
+                    }
+                val context = SSLContext.getInstance("TLS").apply {
+                    init(null, tmf.trustManagers, null)
+                }
+                sslSocketFactory = context.socketFactory
+            }
+        }
         val reader = JsonReader(InputStreamReader(urlConnection.inputStream, "utf-8"))
 
         try {
@@ -169,7 +179,40 @@ class DownloadNexTripsTask(private val mDownloadedListener: OnDownloadedListener
     }
 
     companion object {
-        private val NEXTRIPS_URL = "svc.metrotransit.org/NexTrip/"
-        private var mUseHttps = true
+        private val NEXTRIPS_URL = "https://svc.metrotransit.org/NexTrip/"
+        private val SERVER_CERTIFICATE =
+"""-----BEGIN CERTIFICATE-----
+MIIFvDCCBKSgAwIBAgIQEHEGWQaQjqn4GvYBlPTznTANBgkqhkiG9w0BAQsFADCB
+ljELMAkGA1UEBhMCR0IxGzAZBgNVBAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4G
+A1UEBxMHU2FsZm9yZDEaMBgGA1UEChMRQ09NT0RPIENBIExpbWl0ZWQxPDA6BgNV
+BAMTM0NPTU9ETyBSU0EgT3JnYW5pemF0aW9uIFZhbGlkYXRpb24gU2VjdXJlIFNl
+cnZlciBDQTAeFw0xODAzMDgwMDAwMDBaFw0yMDAyMTkyMzU5NTlaMIGqMQswCQYD
+VQQGEwJVUzETMBEGA1UEERMKNTUxMDEtMTgwNTELMAkGA1UECBMCTU4xETAPBgNV
+BAcTCFN0LiBQYXVsMR0wGwYDVQQJExQzOTAgUm9iZXJ0IFN0LiBOb3J0aDEdMBsG
+A1UEChMUTWV0cm9wb2xpdGFuIENvdW5jaWwxCzAJBgNVBAsTAlJBMRswGQYDVQQD
+DBIqLm1ldHJvdHJhbnNpdC5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+AoIBAQDD3olzlnHMedflum3NgbrYyhpOau04tTPzwKg14cphWGpfnkdLa2vJXte+
+1dZ4VN8kievHqTGcfjDKYEoXEqrTx16G6+5f7WvFq3RH+auTTZ5KHluWGA/wVCjq
+byS5uy1Xg7GE3AF8OF8FpilET1C3OGCBndhgiRs/d5fCJE9a9WXIfqNEXDW4f/2m
++GHgbJjKCAkm08F7CTPLNQEYdN4K3ECg+a3/NjWraQUCThDE/B2HfVZdDF6ZNSLH
+PxSpjbB+l15MLKd57sV6fhAdt3WqKksYtEwgy0qrHLX5EaqQsp+eimKvOGju8BZ2
+u32gdNQepIvl52GYPt94uPpmv87VAgMBAAGjggHuMIIB6jAfBgNVHSMEGDAWgBSa
+8yvaz61Pti+7KkhIKhK3G0LBJDAdBgNVHQ4EFgQU6P0mHQenUc2NC1aZ1/8aro2y
+e3gwDgYDVR0PAQH/BAQDAgWgMAwGA1UdEwEB/wQCMAAwHQYDVR0lBBYwFAYIKwYB
+BQUHAwEGCCsGAQUFBwMCMFAGA1UdIARJMEcwOwYMKwYBBAGyMQECAQMEMCswKQYI
+KwYBBQUHAgEWHWh0dHBzOi8vc2VjdXJlLmNvbW9kby5jb20vQ1BTMAgGBmeBDAEC
+AjBaBgNVHR8EUzBRME+gTaBLhklodHRwOi8vY3JsLmNvbW9kb2NhLmNvbS9DT01P
+RE9SU0FPcmdhbml6YXRpb25WYWxpZGF0aW9uU2VjdXJlU2VydmVyQ0EuY3JsMIGL
+BggrBgEFBQcBAQR/MH0wVQYIKwYBBQUHMAKGSWh0dHA6Ly9jcnQuY29tb2RvY2Eu
+Y29tL0NPTU9ET1JTQU9yZ2FuaXphdGlvblZhbGlkYXRpb25TZWN1cmVTZXJ2ZXJD
+QS5jcnQwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmNvbW9kb2NhLmNvbTAvBgNV
+HREEKDAmghIqLm1ldHJvdHJhbnNpdC5vcmeCEG1ldHJvdHJhbnNpdC5vcmcwDQYJ
+KoZIhvcNAQELBQADggEBABCGppVgD6PHNAzdoy1gHtvoJEwYmoTclPZTqFb80sX9
+aKiwogXGZhuBprfv6IevW897lKd0cm7/Vze4O8raU0eVA/S6t6a+KLTQ8x8oi4yr
+uqeaphBKwBsiM3k6QxPNsEcPfH3DMAGXnOvYtoPP1bW6GUG4MGbhJJq8/4ZQft20
+RA19x6ALEbk3NZf359c3MUcm6hM0mL3rhimo3bnlI7aoWbmnweLqGbXaxkmB1+0w
+Re5CC3678BMa+CX45pXNxAuL2UbJzjEyS3KQVtaqUTSbDCVLsNcZfEsdiwN5JL4o
+AbmzNlB7SmQ4YwT66SxKHunjBEI/ZK8lCJtjGOPYsrs=
+-----END CERTIFICATE-----"""
     }
 }
