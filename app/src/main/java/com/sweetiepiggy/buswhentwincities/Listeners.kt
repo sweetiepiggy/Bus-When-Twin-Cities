44157636fbcbc4c63048ea4f4eb3dc20dfbf77ee
Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
2019-10-09 07:15:48 -0500

    commit Listeners.kt

---
+++ app/src/main/java/com/sweetiepiggy/buswhentwincities/Listeners.kt
@@ -0,0 +1,29 @@
+/*
+    Copyright (C) 2019 Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
+
+    This file is part of Bus When? (Twin Cities).
+
+    Bus When? (Twin Cities) is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Bus When? (Twin Cities) is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Bus When? (Twin Cities); if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.sweetiepiggy.buswhentwincities
+
+interface OnDownloadErrorListener {
+    fun onDownloadError(err: MetroTransitDownloader.DownloadError)
+}
+
+interface OnChangeRefreshingListener {
+    fun setRefreshing(refreshing: Boolean)
+}
+
